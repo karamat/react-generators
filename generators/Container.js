553e1b69a504f14a8bd9b111382133f73c1a2cc2
Karamat Ullah <karamatullah46@gmail.com>
2018-04-14 14:57:53 +0500

    moved all templates into defualt project directory

---
+++ generators/Container.js
@@ -0,0 +1,37 @@
+/**
+ * Component Generator
+ */
+
+'use strict';
+
+const { containersDir } = require('../../')
+
+module.exports = {
+  description: 'Add a container',
+  prompts: [{
+    type: 'input',
+    name: 'name',
+    message: 'What should it be called?',
+    default: 'NewContainer',
+    validate: (value) => {
+      if ((/.+/).test(value)) {
+        return true
+      }
+      return 'The name is required'
+    },
+  }],
+  actions: (data) => {
+    const actions = [{
+      type: 'add',
+      path: `${containersDir}/{{properCase name}}/index.js`,
+      templateFile: './Container/es6.js.hbs',
+      abortOnFail: true,
+    }, {
+      type: 'add',
+      path: `${containersDir}/{{properCase name}}/styles.js`,
+      templateFile: './Container/styles.js.hbs',
+      abortOnFail: true,
+    }]
+    return actions
+  },
+}
---
+++ generators/ReduxAction.js
@@ -0,0 +1,113 @@
+/**
+ * Redux Entity Generator
+//  */
+//
+// 'use strict';
+const fs = require('fs');
+const path = require('path');
+const { customModify, regex, trimFile, getReduxEntities,
+  getContainers } = require('../Utils')
+const { reduxDir, containersDir } = require('../../')
+
+const trimFileFunc = (file) => trimFile(__dirname, file)
+
+module.exports = {
+  description: 'Add a redux action',
+  prompts: [{
+    type: 'input',
+    name: 'actionName',
+    message: 'What is the action name?',
+    validate: (value) => {
+      if ((/.+/).test(value)) {
+        return true
+      }
+      return 'The action name is required'
+    },
+  }, {
+    type: 'confirm',
+    name: 'wantRequestActions',
+    message: 'Do you want to create actions with postfix Request, Success, Failure?',
+  }, {
+    type: 'list',
+    name: 'reduxFileName',
+    message: 'Which redux file should it be added to?',
+    choices: getReduxEntities(),
+    default: 0,
+    validate: (value) => {
+      if ((/.+/).test(value)) {
+        return true
+      }
+      return 'The file name is required'
+    },
+  }, {
+    type: 'list',
+    name: 'containerFileName',
+    message: 'Which container file should it be added to?',
+    choices: getContainers(),
+    default: 0,
+    validate: (value) => {
+      if ((/.+/).test(value)) {
+        return true
+      }
+      return 'The file name is required'
+    },
+  }],
+  // }, {
+  //   type: 'input',
+  //   name: 'containerFileName',
+  //   message: 'Which container file should it be added to?',
+  // }],
+  actions: (data) => {
+    let actions = [{
+      type: 'modify',
+      path: `${reduxDir}/{{reduxFileName}}.js`,
+      pattern: /(\s*\/\/\sadd action here)/g,
+      template: trimFileFunc('addAction.hbs'),
+    }, {
+      type: 'modify',
+      path: `${reduxDir}/{{reduxFileName}}.js`,
+      pattern: /(\s*\/\/\sadd new reducer here)/g,
+      template: trimFileFunc('addReducer.hbs'),
+    }, {
+      type: 'modify',
+      path: `${reduxDir}/{{reduxFileName}}.js`,
+      pattern: /(\s*\/\/\sadd reducer hook up here)/g,
+      template: trimFileFunc('addReducerHook.hbs'),
+    }]
+
+
+    const actionTemplate = (data.wantRequestActions
+                            ? 'promiseActionToProps.hbs'
+                            : 'actionToProps.hbs')
+
+    const containerFile = `${containersDir}/${data.containerFileName}/index.js`
+    const mapActionToProps = customModify(data, {
+      path: containerFile,
+      pattern: new RegExp(regex.mapActionToProps, 'g'),
+      template: trimFileFunc(actionTemplate),
+      dataMapping: {
+        retainPattern: 'actionPattern',
+        actionName: data.actionName,
+        actionArgs: 'payload',
+      },
+      escape: ['=', '>']
+    });
+
+    const propTypes = customModify(data, {
+      path: containerFile,
+      pattern: new RegExp(regex.propTypes, 'g'),
+      template: trimFileFunc('./propTypes.hbs'),
+      dataMapping: {
+        retainPattern: 'propTypePattern',
+        state: data.actionName,
+        propType: 'func', // or bool or array or func
+        propTypeRequired: true,
+      },
+      escape: ['=']
+    });
+    actions = actions.concat(mapActionToProps);
+    actions = actions.concat(propTypes);
+
+    return actions
+  },
+}
